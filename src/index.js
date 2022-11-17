78103779865224381e035d00ece1d82640bd1e05
Sydney Hopson <sydneyhopsonreignmobile@Sydneys-iMac.local>
2022-11-16 19:47:07 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -63,12 +63,37 @@ contact.children[3].textContent = siteContent.contact['email']
 // TOP CONTENT
 const topContent = document.querySelector('.top-content')
 topContent.children[0].children[0].textContent = siteContent['main-content']['features-h4']
-topContent.children[0].children[0].textContent = siteContent['main-content']['features-content']
+topContent.children[0].children[1].textContent = siteContent['main-content']['features-content']
 topContent.children[1].children[0].textContent = siteContent['main-content']['about-h4']
-topContent.children[0].children[0].textContent = siteContent['main-content']['about-content']
+topContent.children[1].children[1].textContent = siteContent['main-content']['about-content']
 
 //BottomContent
 const bottomContent = document.querySelector('.bottom-content')
+const h4sBottom = bottomContent.querySelectorAll('h4')
+h4sBottom[0].textContent = siteContent['main-content']['services-h4']
+h4sBottom[1].textContent = siteContent['main-content']['product-h4']
+h4sBottom[2].textContent = siteContent['main-content']['vision-h4']
+const pBottom = bottomContent.querySelectorAll('p')
+pBottom[0].textContent = siteContent['main-content']['services-content']
+pBottom[1].textContent = siteContent['main-content']['product-content']
+pBottom[2].textContent = siteContent['main-content']['vision-content']
+
+//CALLTOACTION
+document.querySelector('.cta .cta-text h1 ').textContent = siteContent.cta.h1
+document.querySelector('.cta .cta-text button ').textContent = siteContent.cta.button
+
+//NAVIGATION
+const navLinks = document.querySelectorAll('header nav a')
+const navLinkTexts = Object.values(siteContent.nav) 
+navLinks.forEach((link, index) => {
+  link.textContent = navLinkTexts[index]
+  link.classList.add('italic')
+})
+
+
+
+
+
 
 
 
